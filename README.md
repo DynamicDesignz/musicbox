faf16dedaf50a2047c78f97276c87b2c58d744ab
Alec Smecher <alec@smecher.bc.ca>
2015-09-29 22:55:53 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # musicbox
 An RFID-based control-free music player using a Raspberry Pi.
+
+Uses an ID-12LA RFID chip to load playlists based on swiped tags.
+
+See http://www.cassettepunk.com for a full build log.
---
+++ musicbox.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# musicbox.sh
+# Copyright (c) 2015 by Alec Smecher
+#
+# Set up the music box processes:
+# - Run mplayer in slave mode (listening for commands on a FIFO)
+# - Run the musicbox program to translate RFIDs into playlist loads
+
+mkfifo /tmp/mplayercontrol
+amixer set PCM 75%
+mplayer -slave -idle -af pan=1:0.5:0.5 -input file=/tmp/mplayercontrol &
+stty -F /dev/ttyAMA0 9600 raw
+/home/pi/musicbox/musicbox
